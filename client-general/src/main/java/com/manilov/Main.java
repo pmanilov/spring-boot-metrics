6b6db1b2c9881f288183250991b03419d89672cc
p.manilov <pavman0123456789@gmail.com>
2024-04-01 22:49:53 +0300

    Switch to virtual threads

--- client-general/src/main/java/com/manilov/Main.java
+++ client-general/src/main/java/com/manilov/Main.java
@@ -16,10 +16,7 @@ import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 public class Main {
     private final static Long PERIOD = 2L;
@@ -31,94 +28,115 @@ public class Main {
     private final static String QUEUE_NAME_AMQP = "metricsQueue";
 
     public static void main(String[] args) {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(8);
 
-        Runnable taskHTTP = () -> {
-            RequestBody formBody = new FormBody.Builder()
-                    .add("time", String.valueOf(System.currentTimeMillis()))
-                    .build();
-            Request request = new Request.Builder()
-                    .url("http://localhost:8080/")
-                    .post(formBody)
-                    .build();
-            try {
-                Response response = okHttpClient.newCall(request).execute();
-                //String stringResponse = response.body().string();
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        };
-
-        long initialDelay = 0;
-
+        Runnable taskHTTP = getTaskHTTP();
         for (int i = 0; i < COUNT_CLIENTS; i++) {
-            executorService.scheduleAtFixedRate(taskHTTP, initialDelay, PERIOD, TimeUnit.SECONDS);
+            Thread.startVirtualThread(taskHTTP);
         }
 
         for (int i = 0; i < COUNT_CLIENTS; i++) {
             final String clientId = CLIENT_ID_PREFIX_MQTT + i;
-            Runnable taskMQTT = () -> {
-                try (MqttDefaultFilePersistence persistence = new MqttDefaultFilePersistence("tmpFiles");
-                     MqttClient client = new MqttClient(BROKER_MQTT, clientId, persistence)) {
-                    MqttConnectOptions connOpts = new MqttConnectOptions();
-                    connOpts.setCleanSession(true);
-
-                    System.out.println("Connecting to broker: " + BROKER_MQTT);
-                    client.connect(connOpts);
-                    System.out.println("Connected");
-
-                    while (client.isConnected()) {
-                        String message = String.valueOf(System.currentTimeMillis());
-                        MqttMessage mqttMessage = new MqttMessage(message.getBytes());
-                        mqttMessage.setQos(0);
-                        //System.out.println("Publishing message: " + message);
-                        client.publish(TOPIC_MQTT, mqttMessage);
-                        System.out.println("Message published: " + message);
-                        TimeUnit.SECONDS.sleep(PERIOD);
-                    }
-                } catch (InterruptedException | MqttException e) {
-                    System.err.println(e.getMessage());
-                }
-            };
-            new Thread(taskMQTT).start();
+            Runnable taskMQTT = getTaskMQTT(clientId);
+            Thread.startVirtualThread(taskMQTT);
         }
 
+        Runnable taskCoAP = getTaskCoAP();
         for (int i = 0; i < COUNT_CLIENTS; i++) {
-            Runnable taskCoAP = () -> {
-                CoapConfig.register();
-                CoapClient coapClient = new CoapClient(COAP_URL);
+            Thread.startVirtualThread(taskCoAP);
+        }
+
+        Runnable taskAMQP = getTaskAMQP();
+        for (int i = 0; i < COUNT_CLIENTS; i++) {
+            Thread.startVirtualThread(taskAMQP);
+        }
+    }
+
+    private static Runnable getTaskMQTT(String clientId) {
+        return () -> {
+            try (MqttDefaultFilePersistence persistence = new MqttDefaultFilePersistence("tmpFiles");
+                 MqttClient client = new MqttClient(BROKER_MQTT, clientId, persistence)) {
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+
+                System.out.println("Connecting to broker: " + BROKER_MQTT);
+                client.connect(connOpts);
+                System.out.println("Connected");
+
+                while (client.isConnected()) {
+                    String message = String.valueOf(System.currentTimeMillis());
+                    MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+                    mqttMessage.setQos(0);
+                    //System.out.println("Publishing message: " + message);
+                    client.publish(TOPIC_MQTT, mqttMessage);
+                    System.out.println("Message published: " + message);
+                    TimeUnit.SECONDS.sleep(PERIOD);
+                }
+            } catch (InterruptedException | MqttException e) {
+                System.err.println(e.getMessage());
+            }
+        };
+    }
+
+    private static Runnable getTaskCoAP() {
+        return () -> {
+            CoapConfig.register();
+            CoapClient coapClient = new CoapClient(COAP_URL);
+            try {
+                while (!Thread.interrupted()) {
+                    String payload = String.valueOf(System.currentTimeMillis());
+                    CoapResponse response = coapClient.post(payload, 0);
+                    System.out.println("POST Response: " + response.getResponseText());
+                    TimeUnit.SECONDS.sleep(PERIOD);
+                }
+            } catch (ConnectorException | IOException | InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+            coapClient.shutdown();
+        };
+    }
+
+    private static Runnable getTaskHTTP() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        return () -> {
+            while (!Thread.interrupted()) {
+                RequestBody formBody = new FormBody.Builder()
+                        .add("time", String.valueOf(System.currentTimeMillis()))
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://localhost:8080/")
+                        .post(formBody)
+                        .build();
                 try {
-                    while (!Thread.interrupted()) {
-                        String payload = String.valueOf(System.currentTimeMillis());
-                        CoapResponse response = coapClient.post(payload, 0);
-                        System.out.println("POST Response: " + response.getResponseText());
-                        TimeUnit.SECONDS.sleep(PERIOD);
-                    }
-                } catch (ConnectorException | IOException | InterruptedException e) {
+                    Response response = okHttpClient.newCall(request).execute();
+                    //String stringResponse = response.body().string();
+                } catch (IOException e) {
                     System.err.println(e.getMessage());
                 }
-                coapClient.shutdown();
-            };
-            new Thread(taskCoAP).start();
-        }
+                try {
+                    TimeUnit.SECONDS.sleep(PERIOD);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
+
+    private static Runnable getTaskAMQP() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        for (int i = 0; i < COUNT_CLIENTS; i++) {
-            new Thread(() -> {
-                try (Connection connection = factory.newConnection();
-                     Channel channel = connection.createChannel()) {
-                    channel.queueDeclare(QUEUE_NAME_AMQP, false, false, false, null);
-                    while (true) {
-                        String message = String.valueOf(System.currentTimeMillis());
-                        channel.basicPublish("", QUEUE_NAME_AMQP, null, message.getBytes(StandardCharsets.UTF_8));
-                        System.out.println(" [x] Sent '" + message + "'");
-                        TimeUnit.SECONDS.sleep(PERIOD);
-                    }
-                } catch (TimeoutException | IOException | InterruptedException e) {
-                    System.out.println(e.getMessage());
+        return () -> {
+            try (Connection connection = factory.newConnection();
+                 Channel channel = connection.createChannel()) {
+                channel.queueDeclare(QUEUE_NAME_AMQP, false, false, false, null);
+                while (!Thread.interrupted()) {
+                    String message = String.valueOf(System.currentTimeMillis());
+                    channel.basicPublish("", QUEUE_NAME_AMQP, null, message.getBytes(StandardCharsets.UTF_8));
+                    System.out.println(" [x] Sent '" + message + "'");
+                    TimeUnit.SECONDS.sleep(PERIOD);
                 }
-            }).start();
-        }
+            } catch (TimeoutException | IOException | InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        };
     }
 }
