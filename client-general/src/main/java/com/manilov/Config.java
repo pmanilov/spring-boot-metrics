ce503c918ad159d78099e896cbfd4ca7edd0f7c7
pmanilov <pavman0123456789@gmail.com>
2026-05-13 21:38:15 +0300

    Fix maxInFlight mqtt-quic

--- client-general/src/main/java/com/manilov/Config.java
+++ client-general/src/main/java/com/manilov/Config.java
@@ -23,7 +23,7 @@ public class Config {
         public String metricsHost = "localhost";
         public int metricsPort = 8081;
         public volatile int qos = 0;
-        public int maxInFlight = 8;
+        public int maxInFlight = 32;
         public int publishDrainTimeoutSeconds = 30;
 
         public String brokerUrl() {
@@ -51,7 +51,7 @@ public class Config {
         public String metricsHost = "localhost";
         public int metricsPort = 8083;
         public volatile int qos = 0;
-        public int maxInFlight = 512;
+        public int maxInFlight = 32;
         public int publishDrainTimeoutSeconds = 30;
     }
 }
